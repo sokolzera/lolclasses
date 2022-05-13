b4702f0de5ac06668301d3db9e74b449a3af40b1
3110680 <3110680@TE-GDA-D-D37H09>
2022-05-13 14:17:07 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using LolClasses;
+
+//stwórz nowego champa - garena
+Garen g = new Garen();
+//opdal emotke dla garena
+g.ctrl3();
+
+//stworz instacje przedmiotu - rubycrystal
+BFSword bf = new BFSword();
+//sprawdz ile ma hp garen
+g.showAD();
+//kup mu item
+g.buyItem(bf);
+//sprawdz ile ma teraz hp
+g.showAD();
+//kup mu drugi item
+g.buyItem(bf);
+//sprawdz ile ma teraz hp
+g.showAD();
+
+
+
+
+
+
---
+++ ConsoleApp4/RubyCrystal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolClasses
+{
+    /*
+     * klasa opisująca konkretny rodzaj itemu w sklepie
+     */
+    public class BFSword : Item
+    {
+        //deprecated???
+        //public int bonusHP = 150;
+        public BFSword()
+        {
+            //ustalamy nazwe itemu
+            name = "BFSword";
+            // jego cene
+            price = 1300;
+            //i atrybuty
+            AddStat(Stat.AD, 40);
+        }
+    }
+}
